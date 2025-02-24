6967f4f819c4e6e672af16ec60767798e45f481e
owaisyosuf <owais.251990@gmail.com>
2025-02-24 18:21:41 +0500

    arthmetic operator complete

---
+++ lesson_3_operator_keyword_variables/operator_keyword_variable.py
@@ -0,0 +1,19 @@
+#operator in python
+
+# A:Arithmetic operator
+# B:Assignment operator
+# C:Comparison operator
+# D:Logical operator
+
+# A:Arithmetic operator
+a: int=10
+b: int=20
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a%b) #modulus/remainder
+print(a**b) #exponent/power
+print(a//b) #floor division/remove decimal point
+
+
